998f182cd401fc967733cc5fe9b5ea79758780d6
Dylan Pham <43.dpham@gmail.com>
2022-01-07 13:52:20 -0800

    init

---
+++ bot.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+from twilio.rest import Client
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import time
+
+headers = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"
+
+cred = credentials.Certificate("mercari-bot-337508-ed622aca8fd4.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+collection_ref = db.collection("searches")
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
+my_phone_number = os.enivron['MY_PHONE_NUMBER']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+def send_sms_notif(product_details):
+    title = product_details[0]
+    price = product_details[2]
+    link = product_details[1]
+
+    body = title + " " + price + "\n\nhttps://mercari.com" + link
+    message = client.messages \
+        .create(
+            body=body,
+            from_=twilio_phone_number,
+            to=my_phone_number
+         )
+
+    print(message.sid)
+
+def get_new_listings(url, old_listings):
+    soup = BeautifulSoup(requests.get(doc.get("url"), headers=headers).text, "html.parser")
+
+    search_results = soup.find("div", {"data-testid" : "SearchResults"}).find("div").find("div").find_all("div", recursive=False)
+
+    product_details = dict()
+    current_listings = set()
+    for search_result in search_results:
+        title = search_result.find("a")["alt"]
+        link = search_result.find("a")["href"]
+        price = search_result.find("a").find("div").find_all("div", recursive=False)[1].find_all("div", recursive=False)[1].find("p").find("span").text
+        _id = search_result.find("a").find("div")["data-productid"]
+
+        product_details[_id] = (title, link, price)
+        current_listings.add(_id)
+
+    # updating database with most recent set of listings
+    collection_ref.document(doc.id).update({"listings": current_listings})
+
+    new_listings = current_listings - old_listings
+    return new_listings, product_details
+
+def notif_new_listings(new_listings, product_details):
+    for product_id in new_listings:
+        send_sms_notif(product_details[product_id])
+
+def main():
+    print("ran")
+
+    for doc in collection_ref.stream():
+        new_listings, product_details = get_new_listings(doc.get("url"), doc.get("listings"))
+        notif_new_listings(new_listings, product_details)
+
+while True:
+    main()
+
+    # check for new listings every 15 minutes
+    time.sleep(900)
