99fbf8388e95a8e2d5fe1af76fa4042827435de1
Dylan Pham <dpham@Dylans-MacBook-Air.local>
2022-08-17 20:44:50 -0700

    switch over to selenium

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import time
-
-headers = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"
+from selenium import webdriver
 
 cred = credentials.Certificate("mercari-bot-337508-0a00be491377.json")
 firebase_admin.initialize_app(cred)
@@ -35,17 +34,18 @@ def send_sms_notif(product_details):
             to=my_phone_number
          )
 
-    print(message.sid)
-
 def get_new_listings(doc):
-    soup = BeautifulSoup(requests.get(doc.get("url"), headers=headers).text, "html.parser")
+    browser = webdriver.Safari()
+    browser.get("https://www.mercari.com/search/?keyword=iphone")
+    time.sleep(3)
 
+    soup = BeautifulSoup(browser.page_source, "html.parser")
     search_results = soup.find("div", {"data-testid" : "SearchResults"}).find("div").find("div").find_all("div", recursive=False)
 
     product_details = dict()
     current_listings = set()
     for search_result in search_results:
-        title = search_result.find("a")["alt"]
+        title = search_result.find("a").find("div").find_all("div")[1].find("div").find("div")
         link = search_result.find("a")["href"]
         price = search_result.find("a").find("div").find_all("div", recursive=False)[1].find_all("div", recursive=False)[1].find("p").find("span").text
         _id = search_result.find("a").find("div")["data-productid"]
@@ -66,7 +66,8 @@ def notif_new_listings(new_listings, product_details):
 def main():
     for doc in collection_ref.stream():
         new_listings, product_details = get_new_listings(doc)
-        notif_new_listings(new_listings, product_details)
+        print(new_listings)
+        # notif_new_listings(new_listings, product_details)
 
     print("ran")
 
